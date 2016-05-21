b90a92eb6a47fe3a9fc822a4743ac20f1c3242f3
Pramod Sripada <pramod.sripada@gmail.com>
2016-05-21 14:03:29 -0400

    Modularized the routes into seprate folder

--- app/index.js
+++ app/index.js
@@ -1,15 +1,3 @@
-var router = require('express').Router();
-
-router.get('/', function (req, res) {
-	res.render('login', {
-		pageTitle : 'My Login'
-	});
-});
-
-router.get('/info', function (req, res) {
-	res.send('Test page');
-});
-
 module.exports = {
-	router: router
+	router: require('./routes')()
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 module.exports = function () {
+	console.log("Requestiing this function")
 	let routes = {
 		'get':{
 			'/': function(req, res, next){
@@ -10,7 +11,7 @@ module.exports = function () {
 				res.render('rooms');
 			},
 			'/chat': function (req, res, next) {
-				res.render('chatrooms')
+				res.render('chatroom')
 			}
 		},
 		'post':{
@@ -21,11 +22,23 @@ module.exports = function () {
 	// Iterate through the routes and mount the routes on the express routes middleware
 	// Register the routes on the routers.get // routers.post the express routes provide
 	let registerRoutes = function (routes, method) {
+		console.log("In this registerRoutes function")
 		for(var key in routes) {
-			if(typeof routes[key] === 'object' && routes[key] !== null && !(routes)){
-
+				// Should be object and null and an array
+			if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)){
+				registerRoutes(routes[key], key);
+			} else{
+				// Register the route
+				if(method == 'get'){
+					router.get(key, routes[key]);
+				}else if(method == 'post'){
+					router.post(key, routes[key]);
+				}
 			}
 		}
 	}
+
+	registerRoutes(routes);
+	return router;
 	
 }
